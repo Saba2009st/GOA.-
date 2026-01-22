a416140aa1d476ef814ab5eaaed69a20a4f4f231
Saba2009st <kachkachashvilisaba9@gmail.com>
2026-01-22 17:26:44 +0400

    Add files via upload

---
+++ hw.lvl12.py
@@ -0,0 +1,21 @@
+
+ # 1. Sequencing (თანმიმდევრობა)
+# კოდის შესრულება ხაზ-ხაზად.
+# მაგალითი:
+print("Step 1: Open the door")
+print("Step 2: Walk outside")
+
+# 2. Iteration (გამეორება/ციკლი)
+# როცა ერთი და იგივე მოქმედება ბევრჯერ სრულდება.
+# მაგალითი:
+for i in range(3):
+    print("Hello!") # ეს დაიბეჭდება 3-ჯერ
+
+# 3. Selection (არჩევანი/პირობა)
+# გადაწყვეტილების მიღება if-ის და else-ის დახმარებით.
+# მაგალითი:
+weather = "rainy"
+if weather == "rainy":
+    print("Take an umbrella")
+else:
+    print("You don't need an umbrella") 
